02af6dc700c34decf2d1a277d7830d8e23ab1067
Nicolas Ottone <nicolasottone@hotmail.com>
2024-04-04 00:53:54 -0300

    Plain Logo updated

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,21 +1,16 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-export default function Logo() {
+interface LogoProps {
+  small?: boolean;
+}
+
+export default function Logo({ small }: LogoProps) {
+
   return (
-    <Link href="/" className="block" aria-label="Silicon Mind">
-{/*       <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
-            <stop stopColor="#4FD1C5" offset="0%" />
-            <stop stopColor="#81E6D9" offset="25.871%" />
-            <stop stopColor="#338CF5" offset="100%" />
-          </radialGradient>
-        </defs>
-        <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
-      </svg> */}
-
-      <span className='font-inter font-bold text-xl sm:text-2xl tracking-[0.205em]'>SILICON MIND</span>
-
+    <Link href="/" className="block" aria-label="Silicon Mind AI">
+      <span className={`font-medium ${small ? "text-xl" : "text-2xl"} tracking-[0.18rem] cursor-pointer`}>
+        SiliconMind AI
+      </span>
     </Link>
-  )
+  );
 }
