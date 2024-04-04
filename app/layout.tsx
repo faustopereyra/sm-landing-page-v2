22cc3ebc9b99d1420b07db6b9e6c027d793aae56
Nicolas Ottone <nicolasottone@hotmail.com>
2024-04-04 00:14:03 -0300

    Geist Sans Font added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,28 @@
-import './css/style.css'
+import "./css/style.css";
 
-import { Inter } from 'next/font/google'
-
-import Header from '@/components/ui/header'
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap'
-})
+import { GeistSans } from "geist/font/sans";
+import Header from "@/components/ui/header";
 
 export const metadata = {
-  title: 'SiliconMind AI',
-  description: 'Chatbot Development and AI Automation',
-}
+  title: "SiliconMind AI",
+  description: "Chatbot Development and AI Automation",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}>
+      <body
+        className={`${GeistSans.className} antialiased bg-white text-gray-900 tracking-tight`}
+      >
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           <Header />
           {children}
         </div>
       </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,10 @@ module.exports = {
           900: '#234E52',
         },
       },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
       boxShadow: {
         xs: '0 0 0 1px rgba(0, 0, 0, 0.16)',
         sm: '0 1px 2px 0 rgba(0, 0, 0, 0.16)',
